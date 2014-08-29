6e40d1948647d605c066ada44d24eb3b76bae3a9
Chad <yuzhaobo77@gmail.com>
2014-08-28 23:57:48 -0700

    Add hash functions

--- src/BHashTable.java
+++ src/BHashTable.java
@@ -21,6 +21,11 @@ public class BHashTable<K, V> {
 	// total entries in the hash table
 	private int count;
 
+	// a randomized value applied to MPQ hash
+	private int hashSeed;
+
+	private long cryptTable[];
+
 	/**
 	 * 
 	 */
@@ -65,7 +70,28 @@ public class BHashTable<K, V> {
 
 	private long hash(String key) {
 		long seed1 = 0x7FED7FED, seed2 = 0xEEEEEEEE;
+		for (int i = 0; i < key.length(); i++) {
+			int ch = key.charAt(i);
+			seed1 = cryptTable[(hashSeed << 8) + ch] ^ (seed1 + seed2);
+			seed2 = ch + seed1 + seed2 + (seed2 << 5) + 3;
+		}
 		return 0;
+	}
+
+	private void initCryptTable() {
+		this.cryptTable = new long[0x500];
+		long seed = 0x00100001;
+		int index1 = 0, index2 = 0, i;
+		for (index1 = 0; index1 < 0x100; index1++) {
+			for (index2 = index1, i = 0; i < 5; i++, index2 += 0x100) {
+				long temp1, temp2;
+				seed = (seed * 125 + 3) % 0x2AAAAB;
+				temp1 = (seed & 0xFFFF) << 0x10;
+				seed = (seed * 125 + 3) % 0x2AAAAB;
+				temp2 = (seed & 0xFFFF);
+				cryptTable[index2] = (temp1 | temp2);
+			}
+		}
 	}
 
 }
